bc7310332eb87cef1c2a0ac71581cf448a6a5931
code-me-live <patricebelda@gmail.com>
2023-01-20 20:41:38 +0100

    test

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import win32com.client
 def list_connected_devices():
     wmi = win32com.client.GetObject("winmgmts:")
     devices = wmi.InstancesOf("Win32_PnPEntity")
+    ports = wmi.InstancesOf("Win32_SerialPort")
     data = []
     for device in devices:
         data.append({
             "Device Name": device.Name,
             "Device Description": device.Description,
             "Device Hardware ID": device.HardwareID
+        })
+    for port in ports:
+        data.append({
+            "Port Name": port.Name,
+            "Port Description": port.Description,
+            "Port Status": port.Status
         })
     with open('devices.json', 'w') as json_file:
         json.dump(data, json_file)
